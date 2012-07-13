7b6b3b8ec25e25c97a70565b090a0f18852931f4
Mark Watts <markw@cs.utexas.edu>
2012-07-13 10:20:46 -0500

    Whitespace cleanup

--- log.c
+++ log.c
@@ -33,10 +33,10 @@ void log_open(const char *name, int log_filter)
     // here.  If we can't open the logfile, we're dead.
     _log_file = fopen(name, "w");
     if (_log_file == NULL) {
-	perror("logfile");
-	exit(EXIT_FAILURE);
+        perror("logfile");
+        exit(EXIT_FAILURE);
     }
-    
+
     // set logfile to line buffering
     setvbuf(_log_file, NULL, _IOLBF, 0);
     _logging = 1;
@@ -97,7 +97,7 @@ int log_error (const char *str)
 
     return ret;
 }
-    
+
 void log_pair (gpointer key, gpointer val, gpointer not_used)
 {
     log_msg0("%p=>",  key);
@@ -141,38 +141,38 @@ void log_fi (struct fuse_file_info *fi)
     lock_log();
     /** Open flags.  Available in open() and release() */
     //	int flags;
-	log_struct(fi, flags, 0x%08x, );
-	
+    log_struct(fi, flags, 0x%08x, );
+
     /** Old file handle, don't use */
-    //	unsigned long fh_old;	
-	log_struct(fi, fh_old, 0x%08lx,  );
+    //	unsigned long fh_old;
+    log_struct(fi, fh_old, 0x%08lx,  );
 
     /** In case of a write operation indicates if this was caused by a
-        writepage */
+      writepage */
     //	int writepage;
-	log_struct(fi, writepage, %d, );
+    log_struct(fi, writepage, %d, );
 
     /** Can be filled in by open, to use direct I/O on this file.
-        Introduced in version 2.4 */
+      Introduced in version 2.4 */
     //	unsigned int keep_cache : 1;
-	log_struct(fi, direct_io, %d, );
+    log_struct(fi, direct_io, %d, );
 
     /** Can be filled in by open, to indicate, that cached file data
-        need not be invalidated.  Introduced in version 2.4 */
+      need not be invalidated.  Introduced in version 2.4 */
     //	unsigned int flush : 1;
-	log_struct(fi, keep_cache, %d, );
+    log_struct(fi, keep_cache, %d, );
 
     /** Padding.  Do not use*/
     //	unsigned int padding : 29;
 
     /** File handle.  May be filled in by filesystem in open().
-        Available in all other file operations */
+      Available in all other file operations */
     //	uint64_t fh;
-	log_struct(fi, fh, 0x%016llx,  );
-	
+    log_struct(fi, fh, 0x%016llx,  );
+
     /** Lock owner id.  Available in locking operations and flush */
     //  uint64_t lock_owner;
-	log_struct(fi, lock_owner, 0x%016llx, );
+    log_struct(fi, lock_owner, 0x%016llx, );
     unlock_log();
 };
 
@@ -182,44 +182,44 @@ void log_stat(struct stat *si)
 {
     lock_log();
     //  dev_t     st_dev;     /* ID of device containing file */
-	log_struct(si, st_dev, %lld, );
-	
+    log_struct(si, st_dev, %lld, );
+
     //  ino_t     st_ino;     /* inode number */
-	log_struct(si, st_ino, %lld, );
-	
+    log_struct(si, st_ino, %lld, );
+
     //  mode_t    st_mode;    /* protection */
-	log_struct(si, st_mode, 0%o, );
-	
+    log_struct(si, st_mode, 0%o, );
+
     //  nlink_t   st_nlink;   /* number of hard links */
-	log_struct(si, st_nlink, %d, );
-	
+    log_struct(si, st_nlink, %d, );
+
     //  uid_t     st_uid;     /* user ID of owner */
-	log_struct(si, st_uid, %d, );
-	
+    log_struct(si, st_uid, %d, );
+
     //  gid_t     st_gid;     /* group ID of owner */
-	log_struct(si, st_gid, %d, );
-	
+    log_struct(si, st_gid, %d, );
+
     //  dev_t     st_rdev;    /* device ID (if special file) */
-	log_struct(si, st_rdev, %lld,  );
-	
+    log_struct(si, st_rdev, %lld,  );
+
     //  off_t     st_size;    /* total size, in bytes */
-	log_struct(si, st_size, %lld,  );
-	
+    log_struct(si, st_size, %lld,  );
+
     //  blksize_t st_blksize; /* blocksize for filesystem I/O */
-	log_struct(si, st_blksize, %ld,  );
-	
+    log_struct(si, st_blksize, %ld,  );
+
     //  blkcnt_t  st_blocks;  /* number of blocks allocated */
-	log_struct(si, st_blocks, %lld,  );
+    log_struct(si, st_blocks, %lld,  );
 
     //  time_t    st_atime;   /* time of last access */
-	log_struct(si, st_atime, 0x%08lx, );
+    log_struct(si, st_atime, 0x%08lx, );
 
     //  time_t    st_mtime;   /* time of last modification */
-	log_struct(si, st_mtime, 0x%08lx, );
+    log_struct(si, st_mtime, 0x%08lx, );
 
     //  time_t    st_ctime;   /* time of last status change */
-	log_struct(si, st_ctime, 0x%08lx, );
-	
+    log_struct(si, st_ctime, 0x%08lx, );
+
     unlock_log();
 }
 
@@ -227,49 +227,49 @@ void log_statvfs(struct statvfs *sv)
 {
     lock_log();
     //  unsigned long  f_bsize;    /* file system block size */
-	log_struct(sv, f_bsize, %ld, );
-	
+    log_struct(sv, f_bsize, %ld, );
+
     //  unsigned long  f_frsize;   /* fragment size */
-	log_struct(sv, f_frsize, %ld, );
-	
+    log_struct(sv, f_frsize, %ld, );
+
     //  fsblkcnt_t     f_blocks;   /* size of fs in f_frsize units */
-	log_struct(sv, f_blocks, %lld, );
-	
+    log_struct(sv, f_blocks, %lld, );
+
     //  fsblkcnt_t     f_bfree;    /* # free blocks */
-	log_struct(sv, f_bfree, %lld, );
-	
+    log_struct(sv, f_bfree, %lld, );
+
     //  fsblkcnt_t     f_bavail;   /* # free blocks for non-root */
-	log_struct(sv, f_bavail, %lld, );
-	
+    log_struct(sv, f_bavail, %lld, );
+
     //  fsfilcnt_t     f_files;    /* # inodes */
-	log_struct(sv, f_files, %lld, );
-	
+    log_struct(sv, f_files, %lld, );
+
     //  fsfilcnt_t     f_ffree;    /* # free inodes */
-	log_struct(sv, f_ffree, %lld, );
-	
+    log_struct(sv, f_ffree, %lld, );
+
     //  fsfilcnt_t     f_favail;   /* # free inodes for non-root */
-	log_struct(sv, f_favail, %lld, );
-	
+    log_struct(sv, f_favail, %lld, );
+
     //  unsigned long  f_fsid;     /* file system ID */
-	log_struct(sv, f_fsid, %ld, );
-	
+    log_struct(sv, f_fsid, %ld, );
+
     //  unsigned long  f_flag;     /* mount flags */
-	log_struct(sv, f_flag, 0x%08lx, );
-	
+    log_struct(sv, f_flag, 0x%08lx, );
+
     //  unsigned long  f_namemax;  /* maximum filename length */
-	log_struct(sv, f_namemax, %ld, );
-	
+    log_struct(sv, f_namemax, %ld, );
+
     unlock_log();
 }
 
 void log_utime(struct utimbuf *buf)
 {
     lock_log();
-	//    time_t actime;
-	log_struct(buf, actime, 0x%08lx, );
-	
-	//    time_t modtime;
-	log_struct(buf, modtime, 0x%08lx, );
+    //    time_t actime;
+    log_struct(buf, actime, 0x%08lx, );
+
+    //    time_t modtime;
+    log_struct(buf, modtime, 0x%08lx, );
     unlock_log();
 }
 #endif
