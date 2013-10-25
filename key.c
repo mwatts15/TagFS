8add764114db64d092120c7aa84d16d08a07ca67
Mark Watts <markw@cs.utexas.edu>
2013-10-25 00:59:24 -0500

    new home for tagdb_key_t

---
+++ key.c
@@ -0,0 +1,46 @@
+#include <glib.h>
+#include <assert.h>
+#include "log.h"
+#include "tagdb_util.h"
+#include "key.h"
+
+void print_key (tagdb_key_t k)
+{
+    log_msg("<<");
+    KL(k, i)
+    {
+        log_msg("%ld ", key_ref(k,i));
+    } KL_END;
+    log_msg(">>\n");
+}
+
+tagdb_key_t key_new (void)
+{
+    return g_array_new(TRUE, TRUE, sizeof(key_elem_t));
+}
+
+void key_destroy (tagdb_key_t k)
+{
+    g_array_free(k, TRUE);
+}
+
+key_elem_t key_ref (tagdb_key_t k, int index)
+{
+    return g_array_index(k, key_elem_t, index);
+}
+
+void key_push_end (tagdb_key_t k, key_elem_t e)
+{
+    assert(e != 0);
+    g_array_append_val(k, e);
+}
+
+int key_is_empty (tagdb_key_t k)
+{
+    return (g_array_index(k,key_elem_t,0) == 0);
+}
+
+void key_sort (tagdb_key_t k, GCompareFunc c)
+{
+    g_array_sort(k,c);
+}
---
+++ key.h
@@ -0,0 +1,24 @@
+#ifndef KEY_H
+#define KEY_H
+#include <glib.h>
+
+typedef unsigned long long key_elem_t;
+typedef GArray *tagdb_key_t;
+
+/* key for untagged files */
+#define UNTAGGED 0ll
+
+void print_key (tagdb_key_t k);
+#define KL(key, i) \
+    for (int i = 0; key_ref(key, i) != 0; i++)
+
+tagdb_key_t key_new (void);
+void key_destroy (tagdb_key_t k);
+void key_push_end (tagdb_key_t k, key_elem_t e);
+key_elem_t key_ref (tagdb_key_t k, int index);
+int key_is_empty (tagdb_key_t k);
+void key_sort (tagdb_key_t k, GCompareFunc c);
+
+#define KL_END
+#endif /* KEY_H */
+
