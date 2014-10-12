71e8fdfe74fcd14b96a3a57303d14dedc9a69fef
Mark Watts <markw@cs.utexas.edu>
2014-10-12 17:20:02 -0500

    Modified sql_exec wrapper to put the error on the caller's location

--- sql.c
+++ sql.c
@@ -2,13 +2,13 @@
 #include "log.h"
 #include "sql.h"
 
-int sql_exec(sqlite3 *db, char *cmd)
+int _sql_exec(sqlite3 *db, char *cmd, const char *file, int line_number)
 {
     char *errmsg = NULL;
     int sqlite_res = sqlite3_exec(db, cmd, NULL, NULL, &errmsg);
     if (sqlite_res != 0)
     {
-        error(errmsg);
+        log_msg1(ERROR, file, line_number, "sqlite3_exec:%s", errmsg);
         sqlite3_free(errmsg);
     }
     return sqlite_res;
--- sql.h
+++ sql.h
@@ -2,9 +2,10 @@
 #define SQL_H
 #include <sqlite3.h>
 
-int sql_exec(sqlite3 *db, char *cmd);
+int _sql_exec(sqlite3 *db, char *cmd, const char *file, int line_number);
 #define sql_prepare(__db, __cmd, __stmt) \
     sqlite3_prepare_v2(__db, __cmd, -1, &(__stmt), NULL)
+#define sql_exec(__db, __cmd) _sql_exec(__db, __cmd, __FILE__, __LINE__)
 
 #endif /* SQL_H */
 
