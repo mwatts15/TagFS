3de9e030c92cce478e2698f2497ca90fd5db3d56
Mark Watts <wattsmark2015@gmail.com>
2015-02-20 10:36:36 -0600

    Added branch name to version string

--- get_version.sh
+++ get_version.sh
@@ -5,6 +5,8 @@ tag=`git tag --points-at $commit | head -n 1`
 if [ $tag ] ; then
     echo $tag
 else
+    branch=`git branch --list | grep '*' | tail -c +3`
+
     for hist in `git log --format=%H`; do
         tag=`git tag --points-at $hist | head -n 1`
         if [ $tag ] ; then
@@ -12,5 +14,9 @@ else
         fi
     done
     abbr_head=`echo $commit|head -c 7`
-    echo $tag+$abbr_head
+    if [ $branch != "master" ] ; then
+        echo $tag-$branch+$abbr_head
+    else
+        echo $tag+$abbr_head
+    fi
 fi
