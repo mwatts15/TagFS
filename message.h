5c1599e539415a128d1d9814694a35451b6c7546
Mark Watts <wattsmark2015@gmail.com>
2015-06-21 18:38:46 -0500

    Added signals for file open and file delete

--- message.h
+++ message.h
@@ -12,10 +12,12 @@ struct MessageSystem {
     int (*send_signal) (MessageConnection *conn, char *signal);
     int (*prepare_signal) (MessageConnection *conn, char *signal);
     void (*add_arg) (MessageConnection *conn, int message_index,
-            int dbus_type, void *arg);
+            int tagdb_type, void *arg);
     void (*send) (MessageConnection *conn, int message_index);
     void (*destroy_message)(MessageConnection *conn, int message_index);
     void (*destroy) (MessageConnection *conn);
 };
+
+#define CALL(__conn, __method, ...) ((__conn)->sys->__method (__conn, __VA_ARGS__))
 
 #endif /* MESSAGE_SYSTEM_H */
--- src/log.c
+++ src/log.c
@@ -23,7 +23,7 @@ int g_log_filtering_level = 0;
 /* Space is left over for additional names to be added at
  * run-time by users. This hasn't yet been implemented
  */
-const char _level_names[10][10] = {"DEBUG", "INFO", "WARN", "ERROR"};
+const char _level_names[10][10] = {"DEBUG", " INFO", " WARN", "ERROR"};
 
 void log_open(const char *name, int log_filter)
 {
