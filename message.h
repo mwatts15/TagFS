d7be665101f59f66c314709cc1ce1f8039d4fefb
Mark Watts <wattsmark2015@gmail.com>
2015-06-29 21:31:34 -0500

    Added documentation for message interface

--- message.h
+++ message.h
@@ -9,12 +9,32 @@ typedef struct {
 
 
 struct MessageSystem {
+    /* Send a signal without any arguments */
     int (*send_signal) (MessageConnection *conn, char *signal);
+
+    /* Prepare a signal for sending later */
     int (*prepare_signal) (MessageConnection *conn, char *signal);
+
+    /* Prepare a call */
+    int (*prepare_call) (MessageConnection *conn, char *receiver,
+            char *interface, char *method);
+
+    /* Perform a method call */
+    int (*call) (MessageConnection *conn, char *target, int message_index,
+            int timeoutInMS, void**result);
+
+    /* Prepare add an argument to a signal or a method */
+    // XXX: Might distinguish between arguments to signals and methods later
     void (*add_arg) (MessageConnection *conn, int message_index,
             int tagdb_type, void *arg);
+
+    /* Send a message previously prepared */
     void (*send) (MessageConnection *conn, int message_index);
+
+    /* Destroy a message that has been prepared previously */
     void (*destroy_message)(MessageConnection *conn, int message_index);
+
+    /* Destroy a message that has been prepared previously */
     void (*destroy) (MessageConnection *conn);
 };
 
