48a6c7bddff7df64469f1d3e04c58c7b2287fd6b
Mark Watts <wattsmark2015@gmail.com>
2015-09-06 13:29:09 -0500

    Added a check for a subfs operation actually existing
    - In tagfs.lc, all calls to a subfs operation ensuer that the operation
      is actually defined within the component there is now a check to

--- include/subfs.h
+++ include/subfs.h
@@ -8,7 +8,11 @@
 typedef int (*subfs_path_check_fn) (const char *path);
 
 struct subfs_component {
+    /** The name of the component */
+    char *name;
+    /** The file system operations that this component implements */
     struct fuse_operations operations;
+    /** The component's path checker */
     subfs_path_check_fn path_checker;
 };
 
@@ -23,5 +27,7 @@ struct fuse_operations *subfs_get_opstruct (const char *path_to_check);
 int subfs_get_path_handler (const char *path);
 gboolean subfs_component_handles_path (subfs_component *c, const char *path);
 subfs_path_check_fn subfs_component_get_path_matcher (subfs_component *comp);
+struct subfs_component *subfs_get_by_path (const char *path_to_check);
+#define subfs_name(subfs_comp) ((subfs_comp)->name)
 
 #endif /* SUBFS_H */
