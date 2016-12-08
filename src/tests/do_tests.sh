cb10b3ed83bc4ed398a3b37c3575ff3e8c1ef06a
Mark Watts <wattsmark2015@gmail.com>
2016-12-07 22:34:27 -0600

    Made unit tests execute in paralell

--- src/tests/do_tests.sh
+++ src/tests/do_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 EXIT_STATUS=0
 if [ $NO_VALGRIND ] ; then
@@ -12,11 +12,10 @@ else
     RUNNER_TYPE=basic
 fi
 
-all_tests=test_*
-tests=${TESTS:-$all_tests}
-test_results_dir="${TEST_RESULTS_DIR:-unit-test-results}"
-mkdir -p "$test_results_dir"
-for x in $tests ; do 
+do_test (){
+    local x=$1
+    local test_results_dir=$2
+    local exist_status=0
     if [ -x $x ] ; then
         echo "--START $x--"
         this_test_result_dir="$test_results_dir/$x"
@@ -32,14 +31,14 @@ for x in $tests ; do
         $VALGRIND_COMMAND ./$x $RUNNER_TYPE > $test_out; last_status=$?
         # -- Calculate failures and show failure output
         if [ $last_status -ne 0 ] ; then
-            EXIT_STATUS=1
+            exit_status=1
             cat $test_out
         fi
 
         if [ ! $NO_VALGRIND ] ; then
             grep --silent -e "ERROR SUMMARY: 0 errors" $valgrind_out; last_status=$?
             if [ $last_status -ne 0 ] ; then
-                EXIT_STATUS=1
+                exit_status=1
                 cat $valgrind_out
             fi
         fi
@@ -55,6 +54,27 @@ for x in $tests ; do
         mv $test_out "$this_test_result_dir/"
         echo "---STOP $x--"
     fi
+    return $exit_status
+}
+
+all_tests=test_*
+tests=${TESTS:-$all_tests}
+test_results_dir="${TEST_RESULTS_DIR:-unit-test-results}"
+mkdir -p "$test_results_dir"
+EXIT_STATUS=0
+
+child_count=0
+for x in $tests ; do 
+    do_test "$x" "$test_results_dir" &
+    child_count=$((child_count + 1))
+done
+
+while [ $child_count -gt 0 ] ; do
+    wait -n
+    if [ $? -ne 0 ] ; then
+        EXIT_STATUS=1
+    fi
+    child_count=$((child_count - 1))
 done
 
 exit $EXIT_STATUS
