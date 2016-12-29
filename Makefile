54d46196d6893cfbfc5f0c0e16dc50679de168ca
Mark Watts <wattsmark2015@gmail.com>
2016-12-28 21:56:43 -0600

    Added tarball dependency generation

--- Makefile
+++ Makefile
@@ -9,8 +9,16 @@ tagfs:
 tags:
 	ctags --langmap=c:.lc.c.h src/*.c src/*.lc include/*.h 
 
+include tagfs.tar.bz2.d
+tagfs.tar.bz2.d: build_source_tarball.sh
+	@./build_source_tarball.sh -l > tagfs.tar.bz2.d
+
 tagfs.tar.bz2:
 	./build_source_tarball.sh
+
+clean:
+	$(RM) tagfs.tar.bz2.d
+	make -C src/ clean
 
 %::
 	make -C src/ $@
--- build_source_tarball.sh
+++ build_source_tarball.sh
@@ -8,24 +8,36 @@ cleanup () {
 
 trap cleanup EXIT 
 
-echo $TMPDIR
+get_version () {
+    TAG_PREFIX=${TAG_PREFIX:-version_}
+    tag=miku_hatsune
+    tag_search_start=$1
 
-TAG_PREFIX=${TAG_PREFIX:-version_}
-tag=miku_hatsune
-tag_search_start=${1:-HEAD}
+    while [ "$tag" -a \( "x${tag#${TAG_PREFIX}}" = "x${tag}" \) ] ; do
+        tag=$(git describe --abbrev=0 "${tag_search_start}" 2>/dev/null)
+        tag_search_start=${tag}
+    done
 
-while [ "$tag" -a \( "x${tag#${TAG_PREFIX}}" = "x${tag}" \) ] ; do
-    tag=$(git describe --abbrev=0 ${tag_search_start} 2>/dev/null)
-    tag_search_start=${tag}
+    tag=${tag#${TAG_PREFIX}}
+
+    if [ ! $tag ] ; then
+        echo No appropriate version tag could be found. Tag some commit like ${TAG_PREFIX}'<version>' to create a versioned tarball >&2
+    fi
+    echo -n $tag
+}
+
+while getopts lv: f ; do
+    case $f in
+    l)  list_deps=1;;
+    v)  VERSION=$f;;
+    \?) echo $USAGE; exit 1;;
+    esac
 done
-tag=${tag#${TAG_PREFIX}}
+shift `expr $OPTIND - 1`
 
-if [ ! $tag ] ; then
-    echo No appropriate version tag could be found. Tag some commit like ${TAG_PREFIX}'<version>' to create a versioned tarball >&2
-    exit 128
-fi
-
-BASE_NAME="tagfs-${tag}"
+tag_search_start=${1:-HEAD}
+VERSION=${VERSION:-$(get_version "$tag_search_start")}
+BASE_NAME="tagfs-${VERSION:?}"
 
 
 make_files_list () {
@@ -37,8 +49,8 @@ make_files_list () {
 
 FILE_LIST=$(make_files_list)
 
-if [ "$1" = list_deps ] ; then
-    cat $FILE_LIST
+if [ $list_deps ] ; then
+    echo tagfs.tar.bz2: $(echo -n $(cat $FILE_LIST))
 else
     HERE=$(pwd)
 
